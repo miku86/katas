35bde58bee6243f52c9ff8114acd00ec0f2b5483
miku86 <miku86coding@gmail.com>
2018-08-30 16:31:06 +0200

    add one test suite with three cases

--- next-perfect-square/next-perfect-square.js
+++ next-perfect-square/next-perfect-square.js
@@ -1,1 +1,3 @@
-console.log("Hello");
+function findNextSquare(input) {}
+
+module.exports = findNextSquare;
---
+++ next-perfect-square/next-perfect-square.test.js
@@ -0,0 +1,13 @@
+const findNextSquare = require('./next-perfect-square');
+
+it('should input 121 and output 144', () => {
+  expect(findNextSquare(121)).toBe(144);
+});
+
+it('should input 625 and output 676', () => {
+  expect(findNextSquare(625)).toBe(676);
+});
+
+it('should input 114 and output -1', () => {
+  expect(findNextSquare(114)).toBe(-1);
+});
