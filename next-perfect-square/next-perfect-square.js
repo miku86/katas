6286eeb07279433812c873c8572246fde0eae9c2
miku86 <miku86coding@gmail.com>
2018-08-30 16:38:15 +0200

    add function description

--- next-perfect-square/next-perfect-square.js
+++ next-perfect-square/next-perfect-square.js
@@ -1,3 +1,14 @@
-function findNextSquare(input) {}
+/*
+* The function finds the next integral perfect square after the one passed as a parameter.
+* An integral perfect square is an integer n such that sqrt(n) is also an integer.
+* If the parameter is itself not a perfect square, than -1 should be returned.
+* Also check for the input as a number.
+
+* @Input: no specific type
+* @Output: number
+*/
+
+function findNextSquare(input) {
+}
 
 module.exports = findNextSquare;
