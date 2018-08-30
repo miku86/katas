e419c7928cc8843b67cbc3d8faefc61b6742d54a
miku86 <miku86coding@gmail.com>
2018-08-30 17:48:17 +0200

    add pseudocode

--- regex-validate-pin/validate-pin.js
+++ regex-validate-pin/validate-pin.js
@@ -1,3 +1,13 @@
-function validatePin(input) {}
+/*
+* The Function inputs anything and outputs, if the Pin Code contains exactly 4 digits or exactly 6 digits.
+* @Input: any type
+* @Output: Boolean
+*/
+
+
+
+function validatePin(input) {
+  // return if the length of the input is exactly 4 or 6 digits
+}
 
 module.exports = validatePin;
