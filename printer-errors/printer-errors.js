20ee22233e0907edc55d57315eabf53a890006f5
miku86 <miku86coding@gmail.com>
2018-08-30 18:37:36 +0200

    add pseudocode

--- printer-errors/printer-errors.js
+++ printer-errors/printer-errors.js
@@ -1,3 +1,18 @@
-function printerError(input) {}
+/*
+* The Function gets a string with a length greater or equal to one and contains only letters from a to z
+* and will output the error rate of the printer as a string representing a rational whose numerator is the number of errors
+* and the denominator the length of the control string.
+* Correct input characters are from a to m.
+* Don't reduce this fraction to a simpler expression.
+*
+* @Input: String
+* @Output: String
+*/
+
+function printerError(input) {
+  // check length of input letters
+  // check length of invalid input letters
+  // return formatted string
+}
 
 module.exports = printerError;
