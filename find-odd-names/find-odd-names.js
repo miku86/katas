c9d2f26f687739b1392c4456cc8bb6c4559bad10
miku86 <miku86coding@gmail.com>
2018-09-03 14:54:59 +0200

    add real code

--- find-odd-names/find-odd-names.js
+++ find-odd-names/find-odd-names.js
@@ -1,3 +1,15 @@
-function findOddNames(input) {}
+function findOddNames(input) {
+  // create sum of firstName char codes
+  const sumOfCharCode = (name) => {
+    let sumOfChars = 0;
+    for (let i = 0; i < name.length; i++) {
+      sumOfChars += name.charCodeAt(i);
+    }
+    return sumOfChars;
+  };
+
+  // filter array for person with odd firstname
+  return input.filter((person) => sumOfCharCode(person.firstName) % 2 === 1);
+}
 
 module.exports = findOddNames;
