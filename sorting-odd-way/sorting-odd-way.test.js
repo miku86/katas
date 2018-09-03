12fdcb9bd1d6489b4bb4456949ab526146436eb6
miku86 <miku86coding@gmail.com>
2018-09-03 14:21:25 +0200

    add additional test

--- sorting-odd-way/sorting-odd-way.test.js
+++ sorting-odd-way/sorting-odd-way.test.js
@@ -1,5 +1,10 @@
 const sortOddWay = require('./sorting-odd-way');
 
 it("should input [1,2,3,4,5,6,7,8,9] and output [1,3,5,7,9,8,6,4,2]", ()=> {
-  expect(sortOddWay([1, 2, 3, 4, 5, 6, 7, 8, 9])).toBe([1, 3, 5, 7, 9, 8, 6, 4, 2]);
+  expect(sortOddWay([1, 2, 3, 4, 5, 6, 7, 8, 9])).toEqual([1, 3, 5, 7, 9, 8, 6, 4, 2]);
+});
+
+it("should input [11, 22, 33, 44, 55, 55, 90.4, 4, 78] and output [11, 33, 55, 55, 90.4, 78, 44, 22, 4]", ()=> {
+  expect(sortOddWay([11, 22, 33, 44, 55, 55, 90.4, 4, 78]
+  )).toEqual([11, 33, 55, 55, 90.4, 78, 44, 22, 4]);
 });
