d424513855f1a90615faf49d024764457def2dee
miku86 <miku86coding@gmail.com>
2018-09-03 14:26:30 +0200

    add real code

--- sorting-odd-way/sorting-odd-way.js
+++ sorting-odd-way/sorting-odd-way.js
@@ -1,8 +1,17 @@
 function sortOddWay(input) {
-  // find odd numbers
-  // sort them ascending
-  // find even numbers
-  // sort them descending
+  const oddNumbersAscending = input
+    // find odd numbers
+    .filter((num) => Math.floor(num) % 2 === 1)
+    // sort them ascending
+    .sort((a, b) => a - b);
+
+  const evenNumbersDescending = input
+    // find even numbers
+    .filter((num) => Math.floor(num) % 2 === 0)
+    // sort them descending
+    .sort((a, b) => b - a);
+
+  return oddNumbersAscending.concat(evenNumbersDescending);
 }
 
 module.exports = sortOddWay;
