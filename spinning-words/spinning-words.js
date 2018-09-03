87f49e015490dcf963c76ae0adde233ef02f8f8f
miku86 <miku86coding@gmail.com>
2018-09-03 17:21:09 +0200

    add pseudocode

--- spinning-words/spinning-words.js
+++ spinning-words/spinning-words.js
@@ -1,3 +1,8 @@
-function spinWords(input) {}
+function spinWords(input) {
+  // split the input string into separated words
+  // if a word has five or more letters, reverse it
+  // if a word has less than five letters, do nothing
+  // join the words back to a string and return it
+}
 
 module.exports = spinWords;
