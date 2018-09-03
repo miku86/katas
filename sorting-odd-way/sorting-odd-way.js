210f5956becfbf821f0fb4b6530179e6b8a213ca
miku86 <miku86coding@gmail.com>
2018-09-03 14:13:23 +0200

    add pseudocode

--- sorting-odd-way/sorting-odd-way.js
+++ sorting-odd-way/sorting-odd-way.js
@@ -1,3 +1,8 @@
-function sortOddWay(input) {}
+function sortOddWay(input) {
+  // find odd numbers
+  // sort them ascending
+  // find even numbers
+  // sort them descending
+}
 
 module.exports = sortOddWay;
