edf5960644bdfe5e0cabe16b82d72188e18bf7c7
miku86 <miku86coding@gmail.com>
2018-09-03 09:18:36 +0200

    add real code

--- multiples-3-or-5/multiples.js
+++ multiples-3-or-5/multiples.js
@@ -1,8 +1,17 @@
 function multiplesOf3Or5(input) {
+  let results = [];
+
   // create loop from 1 to less than the input number
-  // if the number is a multiple of 3 or 5
-  // then add it to the results
+  for (let num = 1; num <= input - 1; num++) {
+    // if the number is a multiple of 3 or 5
+    if (num % 3 === 0 || num % 5 === 0) {
+      // then add it to the results
+      results.push(num);
+    }
+  }
+
   // create the sum of the results
+  return results.reduce((sum, currentNum) => sum + currentNum, 0);
 }
 
 module.exports = multiplesOf3Or5;
