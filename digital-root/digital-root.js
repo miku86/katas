ae00f95e1ab2a977b0cb6b51c96cf570063d1565
miku86 <miku86coding@gmail.com>
2018-09-03 11:39:22 +0200

    add real code

--- digital-root/digital-root.js
+++ digital-root/digital-root.js
@@ -1,8 +1,22 @@
-function digitalRoot(input){
+function digitalRoot(input) {
+  const sumOfDigits = (num) => {
+    // split digits into array and add them up
+    return [...num.toString()].reduce(
+      (sum, currentNum) => sum + Number(currentNum),
+      0,
+    );
+  };
+
+  let sum = 0;
+
   // while input has more than 1 digit
-    // then sum digits
-  // else (= digit)
-    // then return sum
+  while (input.toString().length > 1) {
+    // then sum digits and reassign sum and input
+    input = sum = sumOfDigits(input);
+  }
+
+  // return sum
+  return sum;
 }
 
 module.exports = digitalRoot;
