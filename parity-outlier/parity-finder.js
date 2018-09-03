b838ef487d12d432d332857fe91b3712349b2506
miku86 <miku86coding@gmail.com>
2018-09-03 12:57:09 +0200

    add real code

--- parity-outlier/parity-finder.js
+++ parity-outlier/parity-finder.js
@@ -1,6 +1,16 @@
 function findParityOutlier(input) {
   // find out if outlier is odd or even
+  const outlierIsOdd = (input) => {
+    // if there is only one number, that is odd, than the outlier is odd, else even
+    return input.filter((num) => num % 2).length === 1;
+  };
+
   // filter out the number that is the outlier
+  return outlierIsOdd(input)
+    // return all odd numbers
+    ? Number(input.filter((num) => num % 2 !== 0))
+    // return all even numbers
+    : Number(input.filter((num) => num % 2 === 0));
 }
 
 module.exports = findParityOutlier;
