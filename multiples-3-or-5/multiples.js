d5adcac94103687e50f65c80db6f18d164d640e4
miku86 <miku86coding@gmail.com>
2018-09-03 09:06:01 +0200

    add pseudocode

--- multiples-3-or-5/multiples.js
+++ multiples-3-or-5/multiples.js
@@ -1,3 +1,8 @@
-function multiplesOf3Or5(input) {}
+function multiplesOf3Or5(input) {
+  // create loop from 1 to less than the input number
+  // if the number is a multiple of 3 or 5
+  // then add it to the results
+  // create the sum of the results
+}
 
 module.exports = multiplesOf3Or5;
