00a5d1ade06b12687325206a8462c795011576f0
miku86 <miku86coding@gmail.com>
2018-09-03 18:02:49 +0200

    add pseudocode

--- who-likes-it/who-likes-it.js
+++ who-likes-it/who-likes-it.js
@@ -1,5 +1,6 @@
-function whoLikesIt(input){
-
+function whoLikesIt(input) {
+  // find length of the input array
+  // switch statement between different amount of numOfLikes
 }
 
 module.exports = whoLikesIt;
