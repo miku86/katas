6143956f61c129d8874889cb26ce4b1602d6f300
miku86 <miku86coding@gmail.com>
2018-09-03 12:28:37 +0200

    add pseudocode

--- parity-outlier/parity-finder.js
+++ parity-outlier/parity-finder.js
@@ -1,3 +1,6 @@
-function findParityOutlier(input) {}
+function findParityOutlier(input) {
+  // find out if outlier is odd or even
+  // filter out the number that is the outlier
+}
 
 module.exports = findParityOutlier;
