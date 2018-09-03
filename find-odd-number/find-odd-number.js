cb7d30e49439ac492d267acfdeef979fe14d1688
miku86 <miku86coding@gmail.com>
2018-09-03 15:29:59 +0200

    add real code

--- find-odd-number/find-odd-number.js
+++ find-odd-number/find-odd-number.js
@@ -1,3 +1,15 @@
-function findOddNumber(input) {}
+function findOddNumber(input) {
+  const numbers = {};
+
+  // find how often the number appears
+  for (let num of input) {
+    typeof numbers[num] === 'undefined' ? (numbers[num] = 1) : numbers[num]++;
+  }
+
+  // return the number of the entry with odd count
+  return Number(
+    Object.entries(numbers).filter((entry) => entry[1] % 2 === 1)[0][0],
+  );
+}
 
 module.exports = findOddNumber;
