51ad9cbae9289a7a91afacdf7bdb272f20dc3622
miku86 <miku86coding@gmail.com>
2018-09-04 09:01:20 +0200

    add real code

--- map-insert-replace/map-insert-replace.js
+++ map-insert-replace/map-insert-replace.js
@@ -1,8 +1,15 @@
 function isolateIt(input) {
   // take every element of the array
-  // If the string length is an even number, use the insert method.
-  // If the string length is an odd number, use the replacement method.
   // return all new strings in an array
+  return input.map((str) => {
+    return str.length % 2 === 0
+      ? // If the string length is an even number, use the insert method.
+        `${str.slice(0, str.length / 2)}|${str.slice(str.length / 2)}`
+      : // If the string length is an odd number, use the replacement method.
+        `${str.slice(0, Math.floor(str.length / 2))}|${str.slice(
+          Math.floor(str.length / 2) + 1,
+        )}`;
+  });
 }
 
 module.exports = isolateIt;
