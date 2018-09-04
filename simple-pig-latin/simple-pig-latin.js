6b09280105dfae64fa651d0fe4b176b087546090
miku86 <miku86coding@gmail.com>
2018-09-04 11:04:21 +0200

    add real code

--- simple-pig-latin/simple-pig-latin.js
+++ simple-pig-latin/simple-pig-latin.js
@@ -1,8 +1,19 @@
-function pigIt(input){
-  // split sentence in its words
-  // move the first letter of each word to the end of it
-  // add "ay" to the end of the word
-  // return joined words
+function pigIt(input) {
+  return (
+    input
+      // split sentence in its words
+      .split(' ')
+      .map((word) => {
+        return word.split('').every((char) => /[a-zA-Z]/.test(char))
+          ? // move the first letter of each word to the end of it
+            // add "ay" to the end of the word
+            `${word.slice(1)}${word.slice(0, 1)}ay`
+          : // leave punctuation marks untouched.
+            word;
+      })
+      // return joined words
+      .join(' ')
+  );
 }
 
 module.exports = pigIt;
--- simple-pig-latin/simple-pig-latin.test.js
+++ simple-pig-latin/simple-pig-latin.test.js
@@ -5,5 +5,5 @@ it('should input "Pig latin is cool" and output "igPay atinlay siay oolcay"', ()
 });
 
 it('should input "Hello world!" and output "elloHay orldWay !"', () => {
-  expect(pigIt('Hello world !')).toBe('elloHay orldWay !');
+  expect(pigIt('Hello world !')).toBe('elloHay orldway !');
 });
