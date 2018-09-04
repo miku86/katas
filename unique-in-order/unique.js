d9b8748005e1bbbb5f153d73a913d042c9da3989
miku86 <miku86coding@gmail.com>
2018-09-04 17:19:46 +0200

    add real code

--- unique-in-order/unique.js
+++ unique-in-order/unique.js
@@ -1,8 +1,21 @@
 function unique(input) {
-  // split input into array
-  // if current item isn't the previous item
-  // add item into new array
+  // if input is empty, return empty array
+  if (input.length < 1) return [];
+  // if input is not array, split input into array
+  let splittedInArray = input;
+  if (typeof input === 'array') splittedInArray = input.split('');
+
+  let result = [];
+  let prevItem;
+
+  for (let i = 0; i < splittedInArray.length; i++) {
+    // if current item isn't the previous item
+    // add item into new array
+    if (splittedInArray[i] !== prevItem) result.push(splittedInArray[i]);
+    prevItem = splittedInArray[i];
+  }
   // return joined array
+  return result;
 }
 
 module.exports = unique;
