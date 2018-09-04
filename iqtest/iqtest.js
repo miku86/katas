7a27074cd3061de7a33fccc6bda1d5402cba51ef
miku86 <miku86coding@gmail.com>
2018-09-04 16:25:03 +0200

    add pseudocode

--- iqtest/iqtest.js
+++ iqtest/iqtest.js
@@ -1,3 +1,9 @@
-function iqTest(input) {}
+function iqTest(input) {
+  // slice string in array
+  // filter for evenness
+  // if length 1,
+  // then return index of first even number plus one
+  // else return index of first odd number plus one
+}
 
 module.exports = iqTest;
