9f3e6993f69d4debb86f2e14d1d1fac1e18c2e91
miku86 <miku86coding@gmail.com>
2018-09-04 16:36:59 +0200

    add real code

--- iqtest/iqtest.js
+++ iqtest/iqtest.js
@@ -1,9 +1,16 @@
 function iqTest(input) {
-  // slice string in array
+  // split string in array
+  const numsInArray = input.split(' ');
   // filter for evenness
-  // if length 1,
-  // then return index of first even number plus one
-  // else return index of first odd number plus one
+  const evenNumsLength = numsInArray.filter((el) => Number(el) % 2 === 0)
+    .length;
+
+  // if length 1
+  return evenNumsLength === 1
+    ? // then return index of first even number plus one
+      numsInArray.indexOf(numsInArray.find((num) => num % 2 === 0)) + 1
+    : // else return index of first odd number plus one
+      numsInArray.indexOf(numsInArray.find((num) => num % 2 === 1)) + 1;
 }
 
 module.exports = iqTest;
