870159a3cef9901796845a1829db54a910942b37
miku86 <miku86coding@gmail.com>
2018-09-04 10:47:44 +0200

    add pseudocode

--- simple-pig-latin/simple-pig-latin.js
+++ simple-pig-latin/simple-pig-latin.js
@@ -1,3 +1,8 @@
-function pigIt(input){}
+function pigIt(input){
+  // split sentence in its words
+  // move the first letter of each word to the end of it
+  // add "ay" to the end of the word
+  // return joined words
+}
 
 module.exports = pigIt;
