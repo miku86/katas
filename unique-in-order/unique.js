fb71cc2120d6ca696cda26e8e45b132823ec3d45
miku86 <miku86coding@gmail.com>
2018-09-04 17:00:42 +0200

    add pseudocode

--- unique-in-order/unique.js
+++ unique-in-order/unique.js
@@ -1,3 +1,8 @@
-function unique(input) {}
+function unique(input) {
+  // split input into array
+  // if current item isn't the previous item
+  // add item into new array
+  // return joined array
+}
 
 module.exports = unique;
