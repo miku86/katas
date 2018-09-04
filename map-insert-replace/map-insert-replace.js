998a578d80da5735da51a0aaa9415139f654d60f
miku86 <miku86coding@gmail.com>
2018-09-04 08:38:23 +0200

    add pseudocode

--- map-insert-replace/map-insert-replace.js
+++ map-insert-replace/map-insert-replace.js
@@ -1,3 +1,8 @@
-function isolateIt(input) {}
+function isolateIt(input) {
+  // take every element of the array
+  // If the string length is an even number, use the insert method.
+  // If the string length is an odd number, use the replacement method.
+  // return all new strings in an array
+}
 
 module.exports = isolateIt;
