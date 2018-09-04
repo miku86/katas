47610ac1369757ca49ba31fc2c8fd85039e6b85a
miku86 <miku86coding@gmail.com>
2018-09-04 11:20:37 +0200

    add pseudocode

--- dubstep/dubstep.js
+++ dubstep/dubstep.js
@@ -1,3 +1,7 @@
-function songDecoder(input){}
+function songDecoder(input) {
+  // replace WUB with space
+  // replace multiple spaces with one space
+  // trim trailing and ending spaces
+}
 
 module.exports = songDecoder;
