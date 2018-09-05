d4ff119da7e81e3e2d29093d66566f708b2610df
miku86 <miku86coding@gmail.com>
2018-09-05 13:29:56 +0200

    add real code

--- find-the-missing-letter/missing-letter.js
+++ find-the-missing-letter/missing-letter.js
@@ -1,9 +1,21 @@
 function findMissingLetter(input) {
   // create array with the inputs' charcodes
+  const charcodes = input.map((char) => char.charCodeAt());
+
   // look at charcode of first element
   // increase charcode + 1
-  // if the charccode is in the array
-  // increase charcode + 1
-  // else return its char
+  let currentCode = charcodes[0] + 1;
+
+  while (true) {
+    // if the charccode is not in the array
+    if (!charcodes.includes(currentCode)) {
+      // return its char
+      return String.fromCharCode(currentCode);
+    } else {
+      // else increase charcode + 1
+      currentCode++;
+    }
+  }
 }
+
 module.exports = findMissingLetter;
