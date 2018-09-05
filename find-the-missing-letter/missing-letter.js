f0eeb8dcae72f2d69a17e536f0fdb77a252ca6ba
miku86 <miku86coding@gmail.com>
2018-09-05 13:12:17 +0200

    add pseudo code

--- find-the-missing-letter/missing-letter.js
+++ find-the-missing-letter/missing-letter.js
@@ -1,4 +1,9 @@
-function findMissingLetter(input){
-
+function findMissingLetter(input) {
+  // create array with the inputs' charcodes
+  // look at charcode of first element
+  // increase charcode + 1
+  // if the charccode is in the array
+  // increase charcode + 1
+  // else return its char
 }
 module.exports = findMissingLetter;
