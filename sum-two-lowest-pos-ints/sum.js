4b95e17ca0a88616bc73ef2b821f8c578c8cfd14
miku86 <miku86coding@gmail.com>
2018-09-05 09:28:31 +0200

    add real code

--- sum-two-lowest-pos-ints/sum.js
+++ sum-two-lowest-pos-ints/sum.js
@@ -1,7 +1,9 @@
 function getSum(input) {
   // sort input ascending
+  const sorted = input.sort((a, b) => a - b);
   // add the first two numbers
   // return the sum
+  return sorted[0] + sorted[1];
 }
 
 module.exports = getSum;
