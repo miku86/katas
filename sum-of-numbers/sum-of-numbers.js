08ea364bcadb05ab5341c76c0ca30739e50463ae
miku86 <miku86coding@gmail.com>
2018-09-05 08:57:39 +0200

    add real code

--- sum-of-numbers/sum-of-numbers.js
+++ sum-of-numbers/sum-of-numbers.js
@@ -1,10 +1,22 @@
-function getSum(input) {
+function getSum(...input) {
   // put the numbers into an array
-  // if the two numbers are equal, return first number
+  let numbers = input;
+
+  // if the two numbers are equal, return the first number
+  if (numbers[0] === numbers[1]) return numbers[0];
+
   // sort the two numbers ascending
+  numbers.sort((a, b) => a - b);
+
   // push all integers between the both numbers, including themselves, into new array
+  let allIntegers = [];
+  for (let num = numbers[0]; num <= numbers[1]; num++) {
+    allIntegers.push(num);
+  }
+
   // create sum
   // return sum
+  return allIntegers.reduce((sum, currentNum) => sum + currentNum, 0);
 }
 
 module.exports = getSum;
