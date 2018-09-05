8beda8cd189d3b12e74bbac336565c9bdb7b98d9
miku86 <miku86coding@gmail.com>
2018-09-05 08:41:38 +0200

    add pseudocode

--- sum-of-numbers/sum-of-numbers.js
+++ sum-of-numbers/sum-of-numbers.js
@@ -1,5 +1,10 @@
-function getSum(input){
-
+function getSum(input) {
+  // put the numbers into an array
+  // if the two numbers are equal, return first number
+  // sort the two numbers ascending
+  // push all integers between the both numbers, including themselves, into new array
+  // create sum
+  // return sum
 }
 
 module.exports = getSum;
