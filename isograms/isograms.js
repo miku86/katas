69418521221df89a105f0eacf850939abf5b3e02
miku86 <miku86coding@gmail.com>
2018-09-05 10:06:23 +0200

    add real code

--- isograms/isograms.js
+++ isograms/isograms.js
@@ -1,6 +1,15 @@
 function isIsogram(input) {
   // count occurency of chars, ignoring letter-case
+  const counter = input
+    .toLowerCase()
+    .split('')
+    .reduce((sum, char) => {
+      sum[char] ? sum[char]++ : (sum[char] = 1);
+      return sum;
+    }, {});
+
   // return if none char has occurency of more than 1
+  return !Object.values(counter).some((value) => value > 1);
 }
 
 module.exports = isIsogram;
